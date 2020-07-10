f09cf739da2a5db380e381073f6fd93b2895eb34
wanghl1993 <whl1994)!!@>
2020-07-10 16:21:32 +0800

    MyFirst Commit --

---
+++ src/main/java/com/imooc/kafkastudy/MyProducer.java
@@ -0,0 +1,8 @@
+package com.imooc.kafkastudy;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+public class MyProducer {
+
+    private static KafkaProducer<String, String> producer;
+}
